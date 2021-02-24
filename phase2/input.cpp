436e8a01e0eefee9db19198fea16a14b68b43ae4
Abhishek262 <abhishek262014@gmail.com>
2021-02-24 22:30:18 +0530

    added phase 2

---
+++ phase2/input.cpp
@@ -0,0 +1,40 @@
+int main()
+{
+
+  int externfs = 10;
+  // scanf("%d",&a);
+  int a[10];
+  if (1)
+  {
+    cout << "hel oe" << endl;
+  }
+  else if (a != 7 && b==5 || x =='5')
+  {
+    cout << "heloe" ;
+  }
+  else
+  {
+    cout << "heloe" << endl;
+  }
+  // else{
+  //     cout << "no";$$
+  // }
+
+  switch (ghd>5)
+  {
+  case 1:
+    // code block
+    int a = 5;
+    break;
+  case 2:
+    // code block
+    int b = 6;
+    // ststring 
+    break;
+  case 3: b = 4;
+
+  default:
+    // code block
+    int c=8;
+  }
+}
